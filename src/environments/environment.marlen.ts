e3506fceef99efd6421a59c0cfc0eab48077cfe5
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2019-02-04 14:19:14 +0100

    config

--- src/environments/environment.marlen.ts
+++ src/environments/environment.marlen.ts
@@ -2,7 +2,7 @@ export const environment = {
   envName: 'Video',
   appId: 'marlen',
   production: true,
-  wssUrl: 'ws://192.168.0.140:8080',
+  wssUrl: 'ws://192.168.2.40:8080',
   domainModes: [{
     "id": "kinder",
     "label": "Kindervideos"
