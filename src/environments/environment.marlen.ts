6623a6646cc255ff927e514dbe48eeaa89e27263
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2020-05-13 22:21:20 +0200

    serverUrl

--- src/environments/environment.marlen.ts
+++ src/environments/environment.marlen.ts
@@ -2,7 +2,7 @@ export const environment = {
   envName: 'Video',
   assetId: 'marlen',
   production: true,
-  serverUrl: 'http://192.168.2.40',
+  serverUrl: 'http://192.168.2.40/php',
   wssUrl: 'ws://192.168.2.40:8080',
   domainModes: [{
     "id": "kinder",
--- src/environments/environment.pw.ts
+++ src/environments/environment.pw.ts
@@ -2,7 +2,7 @@ export const environment = {
   envName: 'Video',
   assetId: 'pw',
   production: true,
-  serverUrl: 'http://192.168.0.150',
+  serverUrl: 'http://192.168.0.150/php',
   wssUrl: 'ws://192.168.0.150:8080',
   domainModes: [{
     "id": "kinder",
